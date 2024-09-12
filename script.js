047beda461c57573ca423de26bc68e7cc9510956
sarayu6 <84723350+sarayu6@users.noreply.github.com>
2024-09-11 20:57:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var i=0;
+var texting='Meet Our Amazing Tutors..!';
+var speed=50;
+function typeWriter(){
+  if(i<texting.length){
+    document.getElementById("txt").innerHTML+=texting.charAt(i);
+    i++;
+    setTimeout(typeWriter,speed);
+  }
+}
+var j=0;
+var speed1=50;
+var teaching='So many reasons to START..!';
+function typing(){
+  if(j<teaching.length){
+    document.getElementById("texts").innerHTML+=teaching.charAt(j);
+    j++;
+    setTimeout(typing,speed1);
+  }
+}
+
+
+
+
+
+
